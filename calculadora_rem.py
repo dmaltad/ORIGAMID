b4899ec58b766d6b311415d060479305e6d743d6
dmaltad <daniel-malt2@hotmail.com>
2026-04-22 17:03:30 -0300

    Exercicio 4

--- calculadora_rem.py
+++ calculadora_rem.py
@@ -1,10 +1,71 @@
 import os
 
-def calcular_rem(pixel):
-    rem =pixel/16
-    print(f'A conversão de {pixel}px equivale a {rem}rem.')
+def limpar_tela():
+    os.system('clear')
 
-os.system('clear')
-print('CONVERSOR DE PIXEL PARA REM\n')
-px = float(input('Digite a quantidade de pixels: '))
-calcular_rem(px)
+def texto_inicial():
+        print('''
+CONVERSOR DE PIXEL PARA REM
+              
+    1. Converter Pixel para Rem
+    2. Converter Rem para Pixel
+    3. Sair
+          
+''')
+        
+def continuar():
+    a = input('Digite alguma coisa para seguir: ')
+
+def calcular_rem():
+    limpar_tela()
+    try:
+        pixel = float(input('Digite a quantidade de pixels: '))
+    except:
+        print('Valor inválido, tente novamente.\n')
+        continuar()
+        calcular_rem()
+    rem =pixel/16
+    print()
+    print(f'A conversão de {pixel}px equivale a {rem}rem.')
+    continuar()
+    main()
+
+def calcular_pixel():
+    limpar_tela()
+    try:
+        rem = float(input('Digite a quantidade de rem: '))
+    except:
+        print('Valor inválido, tente novamente.\n')
+        continuar()
+        calcular_rem()
+    pixel =rem*16
+    print()
+    print(f'A conversão de {rem}rem equivale a {pixel}pixel.')
+    continuar()
+    main()
+
+
+def opcoes():
+    try:
+        opcao = int(input('Digite uma opção: '))
+        
+        match opcao:
+            case 1:
+                calcular_rem()
+            case 2:
+                calcular_pixel()
+            case 3:
+                print('Encerrando...')
+    except:
+        print('Opção inválida, tente novamente.\n')
+        continuar()
+        main()
+        
+def main():
+    limpar_tela()
+    texto_inicial()
+    opcoes()
+
+
+if __name__ == '__main__':
+    main()
